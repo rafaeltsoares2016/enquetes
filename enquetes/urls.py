8ffd7d4311e313b552a501d99ee209fc4d22d631
Rafael Soares <rafaeltsoares2016@gmail.com>
2018-09-05 16:54:29 -0300

    Correção dos bugs e melhorando o template, incluindo o css e o Bootstrap.

--- enquetes/urls.py
+++ enquetes/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'enquetes'
 urlpatterns = [
-	path('', views.index, name='index'),
-	path('<int:id_pergunta>/', views.detalhe, name='detalhe'),
-	path('<int:id_pergunta>/resultados/', views.resultados, name='resultados'),
+	path('', views.IndexView.as_view(), name='index'),
+	path('<int:pk>/', views.DetalheView.as_view(), name='detalhe'),
+	path('<int:pk>/resultados/', views.ResultadosView.as_view(), name='resultados'),
 	path('<int:id_pergunta>/voto/', views.voto, name='voto'),
 ]
--- enquetes/views.py
+++ enquetes/views.py
@@ -1,24 +1,38 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
-from .models import Pergunta
+from .models import Escolha, Pergunta
 
 # Create your views here.
 
-def index(request):
-	lista_ultimas_perguntas = Pergunta.objects.order_by('-data_pub')[:5]
-	template = loader.get_template('enquetes/index.html')
-	context = {
-		'lista_ultimas_perguntas':lista_ultimas_perguntas,
-	}
-	return HttpResponse(template.render(context, request))
+class IndexView(generic.ListView):
+    template_name = 'enquetes/index.html'
+    context_object_name = 'lista_ultimas_perguntas'
 
-def detalhe(request, id_pergunta):
-	return HttpResponse('Você esta olhando a pegunta %s.' % id_pergunta)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Pergunta.objects.order_by('-data_pub')[:5]
 
-def resultados(request, id_pergunta):
-	response = 'Você esta olhando os resultados da pergunta %s.'
-	return HttpResponse(response % id_pergunta)
+class DetalheView(generic.DetailView):
+    model = Pergunta
+    template_name = 'enquetes/detalhe.html'
+
+class ResultadosView(generic.DetailView):
+    model = Pergunta
+    template_name = 'enquetes/resultados.html'
 
 def voto(request, id_pergunta):
-	return HttpResponse('Você esta votando na pergunta %s.' % id_pergunta)
+	pergunta = get_object_or_404(Pergunta, pk=id_pergunta)
+	try:
+		selected_choice = pergunta.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'enquetes/detalhe.html', {
+			'pergunta': pergunta,
+			'error_message': 'Você não selecionou uma opção.',
+			})
+	else:
+		selected_choice.votos += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('enquetes:resultados', args=(id.pergunta,)))
