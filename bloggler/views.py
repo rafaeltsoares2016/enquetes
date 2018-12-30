de758d5e58461fdcd0df3bebcce98f1842935e3e
Rafael Soares <rafaeltsoares2016@gmail.com>
2018-12-30 17:43:11 -0200

    Added view and template for detailed blog post as well as CSS for the site.

--- bloggler/views.py
+++ bloggler/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 def post_list(request): 
     posts = Post.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao') 
     return render(request, 'bloggler/post_list.html', {'posts': posts})
+
+def post_detalhe(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'bloggler/post_detalhe.html', {'post': post})
